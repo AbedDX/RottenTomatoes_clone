bc1ed7379dae8ad557256cf08d7297393c0b652d
Abed <abbe_92@live.com>
2023-12-26 23:40:12 +0200

    docker containerized added

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
 from application import app
+import os
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3001)
+    port = port = int(os.environ.get("PORT", 3001))
+    app.run(host="0.0.0.0", debug=True, port=port)
